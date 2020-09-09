da12f36b8e55734622d97dec18416886120d1e80
Michael Bailey <michaeljaybailey@gmail.com>
2020-09-08 22:15:51 -0700

    fixed axios issue

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,25 +1,5 @@
-// import axios from "axios";
+import axios from "axios";
 
-<<<<<<< HEAD
-// export default {
-//   // Gets all books
-//   getBooks: function() {
-//     return axios.get("/api/books");
-//   },
-//   // Gets the book with the given id
-//   getBook: function(id) {
-//     return axios.get("/api/books/" + id);
-//   },
-//   // Deletes the book with the given id
-//   deleteBook: function(id) {
-//     return axios.delete("/api/books/" + id);
-//   },
-//   // Saves a book to the database
-//   saveBook: function(bookData) {
-//     return axios.post("/api/books", bookData);
-//   }
-// };
-=======
 export default {
   // Gets all recipes
   getRecipes: function() {
@@ -38,4 +18,3 @@ export default {
     return axios.post("/api/recipes", recipeData);
   }
 };
->>>>>>> 9c78884c209762d7237c4c0db6350680e2df1053
