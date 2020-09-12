c061e7331fe332de230b921e96290be9b3424272
Eddie Kim <eddienakkim@gmail.com>
2020-09-12 13:40:58 -0700

    trying to see if creating the customize the color scheme

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { green } from "@material-ui/core/colors";
+
+const theme1 = createMuiTheme({ palette: {
+  primary: green,
+} });
+
 
 ReactDOM.render(
   <React.StrictMode>
+    <MuiThemeProvider theme = { theme1 }>
     <App />
+    </MuiThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
